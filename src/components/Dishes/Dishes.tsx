48eb44c4a71e24774f25af30c7b13cc0a5973451
AkylaiJumabekova <jumabekovaakylai7@gmail.com>
2024-07-21 23:39:02 +0600

    commit

---
+++ src/components/Dishes/Dishes.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import DishItem from './DishItem';
+import { Dish } from '../../types';
+import { addDish } from '../../store/cartSlice';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+  selectDeleteDishLoading,
+  selectDishes,
+  selectFetchDishesLoading,
+} from '../../store/dishesSlice';
+import { deleteDish, fetchDishes } from '../../store/dishesThunks';
+import Spinner from '../Spinner/Spinner';
+
+const Dishes: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const dishes = useAppSelector(selectDishes);
+  const deleteLoading = useAppSelector(selectDeleteDishLoading);
+  const dishesLoading = useAppSelector(selectFetchDishesLoading);
+
+  const addDishToCart = (dish: Dish) => {
+    dispatch(addDish(dish));
+  };
+
+  const removeDish = async (id: string) => {
+    await dispatch(deleteDish(id));
+    await dispatch(fetchDishes());
+  };
+
+  useEffect(() => {
+    dispatch(fetchDishes());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h4>Dishes</h4>
+      {dishesLoading ? (
+        <Spinner />
+      ) : (
+        dishes.map((dish) => (
+          <DishItem
+            key={dish.id}
+            dish={dish}
+            addToCart={() => addDishToCart(dish)}
+            onDelete={() => removeDish(dish.id)}
+            deleteLoading={deleteLoading}
+          />
+        ))
+      )}
+    </>
+  );
+};
+
+export default Dishes;
---
+++ src/containers/NewDish/NewDish.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import DishForm from '../../components/DishForm/DishForm';
+import { ApiDish } from '../../types';
+
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { selectCreateDishLoading } from '../../store/dishesSlice';
+import { createDish } from '../../store/dishesThunks';
+
+const NewDish: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const isCreating = useAppSelector(selectCreateDishLoading);
+
+  const onSubmit = async (dish: ApiDish) => {
+    try {
+      await dispatch(createDish(dish)).unwrap();
+      navigate('/');
+      toast.success('Dish created');
+    } catch (error) {
+      toast.error('Could not create dish!');
+    }
+  };
+
+  return (
+    <div className="row mt-2">
+      <div className="col">
+        <DishForm onSubmit={onSubmit} isLoading={isCreating} />
+      </div>
+    </div>
+  );
+};
+
+export default NewDish;
